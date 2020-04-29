719bcaafbd2869048b169fc31a79eb7c8aa43429
Michał Wlaź <michal.wlaz@orange.com>
2020-04-29 18:39:34 +0200

    host, port env fix

--- utils/constants.js
+++ utils/constants.js
@@ -5,8 +5,8 @@ exports.DB_PORT = process.env.DB_PORT;
 exports.DB_PASSWORD = process.env.DB_PASSWORD;
 exports.DB_USER = process.env.DB_USER;
 exports.DB_NAME = process.env.DB_NAME;
-exports.HOST_PORT = process.env.ALWAYSDATA_HTTPD_PORT;
-exports.HOST_IP = process.env.ALWAYSDATA_HTTPD_IP;
+exports.HOST_PORT = process.env.PORT;
+exports.HOST_IP = process.env.IP;
 exports.HELP_STRING = "Jeśli chcesz rozpocząć nową grę, napisz \"Nowa\". Opcjonalnie możesz wskazać liczbę talii, np. \"Nowa 2\" dla gry z dwiema taliami. Talie nie zawierają jokerów.\nJeśli chcesz dołączyć do gry napisz \"Dołącz id_gry\"";
 exports.HELP_MORE = `Dostępne komendy. Wielkie litery nie mają znaczenia. Parametr w nawiasach okrągłych jest wymagany; parametr w nawiasach kwadratowych jest opcjonalny.
 Aby dowiedzieć się więcej napisz "pomoc (komenda)"
